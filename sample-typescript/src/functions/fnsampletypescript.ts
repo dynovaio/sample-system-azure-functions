b813efbcbee8e98e5931e70e58ed1a1cd2b30d51
zodiacfireworks <martin.vuelta@gmail.com>
2024-07-16 13:02:38 -0500

    Add typescript sample with use cases for instrumentation of knex and azure invocation context logging facility

--- sample-typescript/src/functions/fnsampletypescript.ts
+++ sample-typescript/src/functions/fnsampletypescript.ts
@@ -9,8 +9,10 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function handler(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
     context.error(`Http function processed request for url "${request.url}"`);
-    context.warn(`line Http function processed request for url "${request.url}"`);
-    context.warn("place Http function processed request for url \"%s\"", request.url);
+    context.warn(`Http function processed request for url "${request.url}"`);
+    context.warn("Http function processed request for url \"%s\" (using placeholder subtitution)", request.url);
+
+    console.log(`Http function processed request for url "${request.url}" (usign manual decoration)`, newrelic.agent.getNRLinkingMetadata());
 
     const name = request.query.get('name') || await request.text() || 'world';
 
